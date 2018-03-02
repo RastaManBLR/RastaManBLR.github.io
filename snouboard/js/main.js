939cfffee1b99fb7892fe41469f63e46a61829dd
RastaManBLR <rostoman1992@gmail.com>
2018-03-02 22:37:24 +0300

    2230

--- snouboard/js/main.js
+++ snouboard/js/main.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
   	autoplay: true,
   	loop: true
   	  });
+  $(".slider_snowbords").owlCarousel({
+  	items: 3,
+  	autoplay: true,
+  	loop: true,
+  	nav: true,
+  	center: true,
+  	navText: ['','']
+  	  });
 });
